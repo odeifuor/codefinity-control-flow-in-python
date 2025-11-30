c27c9ada4711c6f3988b4636b52a447bd79556e6
odeifuor <>
2025-11-30 12:54:57 +0000

    Comparison operators

---
+++ mastering_python_if_statements/comparison_operators/main.py
@@ -0,0 +1,11 @@
+street_temperature = 15
+hydration_goal_met = True
+
+running_temp = True
+
+if 10<=running_temp<=20 :
+    print("Are the conditions ideal for running now?", running_temp)
+
+# Hydration goal check
+if hydration_goal_met :
+    print("Great job meeting your hydration goal!")
